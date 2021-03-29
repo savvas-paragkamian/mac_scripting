d424d465f6341a9963f4e180f156bf345ad0ce16
“savvas-paragkamian” <s.paragkamian@gmail.com>
2021-03-29 23:13:13 +0300

    set the repo and connect the scripts

--- README.md
+++ README.md
@@ -6,25 +6,26 @@ This repository contains scripts for the automation of macos and application tas
 
 Mac initiation is possible through HomeBrew. A bash script can help install the basic applications and programming tools. This is a semiautomatic process which is similar to container build files. I split the procedure in 2 distinct parts: applications and programming environment.
 
+### Programming
+
+Programming is diveded in 3 sections
+
+1. coding environment (git, tmux, vim and their source files from my repos to symlink)
+2. programming languages and packages
+3. utilities
+
 ### Applications
 
 1. Install HomeBrew from the website
 2. git clone this repository
 3. run the `brew` script
 
+
+### notes for apps
 djvu app is not in brew
 adobe bridge is not in brew
 dictionary for greek 
 affinity and pixelmator
 speedtest
-
-### Programming environment
-
-1. Set git
-* global credentials and tools
-* clone my dot files repositories
-2. Create symlinks of source files of vim, tmux, bash and zsh
-3. Latex
-4. R and packages
 
 
--- coding_brew.sh
+++ coding_brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd $HOME
+
 # install command line tools
 xcode-select --install
 
@@ -7,11 +9,45 @@ xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew for coding
+# programming environment
+brew install vim
+brew install git
+git config --global user.name "savvas-paragkamian"
+git config --global user.email s.paragkamian@gmail.com
+git config --global core.editor vim
+git config --global init.defaultBranch main
+git config --global credential.helper osxkeychain
+git config --global diff.tool vimdiff
+git config --global merge.tool vimdiff
+git config --global difftool.prompt false
+
+brew install tmux
+brew install htop
+
+git clone https://github.com/savvas-paragkamian/.vim.git
+git clone https://github.com/savvas-paragkamian/.dotfiles.git
+git clone https://github.com/savvas-paragkamian/.tmux.git
+
+# programming languages and packeges
 brew install r
+Rscript -e 'install.packages("tidyverse", repos="https://cloud.r-project.org")'
+
 brew install python
 brew install postgresql
 brew install perl
+brew install gawk
 
+## latex - basic tex
+brew install --cask basictex
+
+## fix path for basictex
+export PATH=$PATH:/Library/TeX/texbin
+
+# utilities
 brew install pandoc
+brew install tesseract
+brew install jq
+brew install imagemagick
 
+# install the apps
+./brew_apps.sh
